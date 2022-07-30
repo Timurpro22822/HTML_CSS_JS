9e5a539277f42bdc370eee897ec3843f3a760ad7
Timurpro22822 <ulanovskytimur@gmail.com>
2022-07-30 14:46:42 +0300

    new

---
+++ HTML CSS JS/JS/01_hometask.js
@@ -0,0 +1,81 @@
+////#region Task 1.1
+// const year = 2022;
+// let dateOfBirth = +prompt("Enter your date of birth:");
+// if(dateOfBirth > year)
+// {
+//     console.log("Error")
+// }
+// else
+// {
+//     console.log(`Username's age is: ${year - dateOfBirth}`);
+// }
+////#endregion
+
+////#region Task 1.2
+// let capacityFD = +prompt("Enter capacity of flash drive in GB:") * 1024;
+// let size = 820;
+// console.log(capacityFD);
+// let result = capacityFD / size
+// alert(`Файлов з розміром в ${size} MB влазить в флешку ${capacityFD / 1024} GB (${capacityFD} MB): ${result}`);
+////#endregion
+
+////#region Task 2.1
+    // let num = +prompt("Enter number:");
+    // let getSymbol = (number) => 
+    // {
+    //     let sym = null;
+    //     switch(number)
+    //     {
+    //         case 1:
+    //             sym = "!";
+    //             break;
+    //         case 2:
+    //             sym = "@";
+    //             break;
+    //         case 3:
+    //             sym = "#";
+    //             break;
+    //         case 4:
+    //             sym = "$";
+    //             break;
+    //         case 5:
+    //             sym = "%";
+    //             break;
+    //         case 6:
+    //             sym = "^";
+    //             break;
+    //         case 7:
+    //             sym = "&";
+    //             break;
+    //         case 8:
+    //             sym = "*";
+    //             break;
+    //         case 9:
+    //             sym = "(";
+    //             break;
+    //         case 0:
+    //             sym = ")";
+    //             break;
+    //         default:
+    //             sym = "Error!";
+    //             break;
+    //     } 
+    //     return sym;
+    // }
+    // alert(`${num} = ${getSymbol(num)}`);
+
+////#endregion
+
+////#region Task 2.2
+    // let year = +prompt("Enter year:");
+    // if(year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
+    // {
+    //     alert("Високосний");
+    //     console.log("Високосний");
+    // }
+    // else{
+    //     alert("Не високосний");
+    //     console.log("Не високосний");
+    // }
+////#endregion
+
